2c8d40fd00b8c307d0062bda224098b4a1974a4b
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-06-10 13:42:42 +0100

    fixed condition and added docs

--- src/perm/orbit.rs
+++ src/perm/orbit.rs
@@ -17,16 +17,21 @@ pub fn orbit_stabilizer(
     let mut traversal: Vec<Rc<Permutation>> = vec![id_ref.clone()];
     // The stablizer.
     let mut stablizer: Vec<Rc<Permutation>> = vec![id_ref.clone()];
+    // While there are still elements of the orbit unused.
     while !to_traverse.is_empty() {
+        //Take an unused element.
         let delta = to_traverse.pop().unwrap();
         for i in 0..gen_set.len() {
             let gamma = gen_set[i].apply(delta);
-            if orbit_set.contains(&gamma) {
+            // If the orbit doesn't contain this value, then add it to the orbit.
+            if !orbit_set.contains(&gamma) {
                 orbit_set.insert(gamma);
                 to_traverse.push(gamma);
+                //element to add to the traversal.
                 let new_traversal_element = traversal[delta].multiply(gen_set[i]);
                 traversal.push(Rc::new(new_traversal_element));
             } else {
+                // Otherwise update the stabilizer
                 let new_stab_elem = traversal[delta]
                     .multiply(gen_set[i])
                     .multiply(&traversal[gamma].inv());
