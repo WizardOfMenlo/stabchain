e15a8dadf5d72bf0f515d6dc91c0eb1a3d390c0c
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-06-11 16:20:35 +0100

    added docs

--- src/perm/rand_perm.rs
+++ src/perm/rand_perm.rs
@@ -10,11 +10,13 @@ pub struct RandPerm {
 }
 
 impl RandPerm {
+    /// Construct and initialise a random permutation generator.
     pub fn from_generators(size: usize, generators: Vec<Permutation>, initial_runs: usize) -> Self {
         let mut rng = rand::thread_rng();
         let mut gen_elements: Vec<Permutation> = generators.clone();
         let k = gen_elements.len();
         for i in (k + 1)..=size {
+            //Repeat elements if there aren't enough generators.
             gen_elements[i] = generators[i - k].clone();
         }
         let a = Permutation::id();
@@ -24,19 +26,24 @@ impl RandPerm {
             gen_elements,
             a,
         };
+        // Inital randomisation.
         for _ in 0..initial_runs {
             rand.random_permutation();
         }
         rand
     }
 
+    /// Generate a random permutation.
     pub fn random_permutation(&mut self) -> Permutation {
         let s = self.rng.gen_range(0, self.size);
         let mut t = s;
+        // Generate another index that isn't equal to s.
         while t == s {
             t = self.rng.gen_range(0, self.size);
         }
+        // Either take product or quotient.
         let e = self.rng.gen_range(-1, 2);
+        //Randomly determine order of operation.
         if self.rng.gen::<f64>() < 0.50 {
             self.gen_elements[s] = self.gen_elements[s].multiply(&self.gen_elements[t].pow(e));
             self.a = self.a.multiply(&self.gen_elements[s]);
