0bbb67fa3622a9cee8796df49187cea7b06bcfdb
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-06-11 14:58:19 +0100

    changed name and added in random permutation function

--- src/perm/rand_perm.rs
+++ src/perm/rand_perm.rs
@@ -2,14 +2,14 @@ use crate::perm::Permutation;
 use rand::rngs::ThreadRng;
 use rand::Rng;
 
-pub struct rand_perm {
+pub struct RandPerm {
     size: usize,
     rng: ThreadRng,
     gen_elements: Vec<Permutation>,
     a: Permutation,
 }
 
-impl rand_perm {
+impl RandPerm {
     pub fn from_generators(size: usize, generators: Vec<Permutation>, initial_runs: usize) -> Self {
         let mut rng = rand::thread_rng();
         let mut gen_elements: Vec<Permutation> = generators.clone();
@@ -18,7 +18,7 @@ impl rand_perm {
             gen_elements[i] = generators[i - k].clone();
         }
         let a = Permutation::id();
-        let mut rand = rand_perm {
+        let mut rand = RandPerm {
             size,
             rng,
             gen_elements,
@@ -30,5 +30,20 @@ impl rand_perm {
         rand
     }
 
-    pub fn random_permutation(&mut self) {}
+    pub fn random_permutation(&mut self) -> Permutation {
+        let s = self.rng.gen_range(0, self.size);
+        let mut t = s;
+        while t == s {
+            t = self.rng.gen_range(0, self.size);
+        }
+        let e = self.rng.gen_range(-1, 2);
+        if self.rng.gen::<f64>() < 0.50 {
+            self.gen_elements[s] = self.gen_elements[s].multiply(&self.gen_elements[t].pow(e));
+            self.a = self.a.multiply(&self.gen_elements[s]);
+        } else {
+            self.gen_elements[s] = self.gen_elements[t].pow(e).multiply(&self.gen_elements[s]);
+            self.a = self.gen_elements[s].multiply(&self.a);
+        }
+        self.a.clone()
+    }
 }
