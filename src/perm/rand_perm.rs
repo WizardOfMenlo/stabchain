8aa8aafa2c296f5fae91e72ead27af3d4efb7571
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-06-12 10:49:47 +0100

    fixed out of bounds error

--- src/perm/rand_perm.rs
+++ src/perm/rand_perm.rs
@@ -1,7 +1,7 @@
 use crate::perm::Permutation;
 use rand::rngs::ThreadRng;
 use rand::Rng;
-
+use std::cmp::max;
 pub struct RandPerm {
     size: usize,
     rng: ThreadRng,
@@ -11,15 +11,26 @@ pub struct RandPerm {
 
 impl RandPerm {
     /// Construct and initialise a random permutation generator.
-    pub fn from_generators(size: usize, generators: Vec<Permutation>, initial_runs: usize) -> Self {
-        let mut rng = rand::thread_rng();
+    /// ```
+    /// use stabchain::perm::Permutation;
+    /// use stabchain::perm::rand_perm::RandPerm;
+    /// let generators : Vec<Permutation> = vec![Permutation::from(vec![1, 0]), Permutation::from(vec![0, 2, 3, 1])];
+    /// let rand_perm = RandPerm::from_generators(11, generators, 50);
+    /// ```
+    pub fn from_generators(
+        min_size: usize,
+        generators: Vec<Permutation>,
+        initial_runs: usize,
+    ) -> Self {
+        let rng = rand::thread_rng();
         let mut gen_elements: Vec<Permutation> = generators.clone();
         let k = gen_elements.len();
-        for i in (k + 1)..=size {
-            //Repeat elements if there aren't enough generators.
-            gen_elements[i] = generators[i - k].clone();
+        //Repeat elements if there aren't enough generators.
+        for i in k..min_size {
+            gen_elements.push(generators[(i - k) % k].clone());
         }
         let a = Permutation::id();
+        let size = max(min_size, k);
         let mut rand = RandPerm {
             size,
             rng,
