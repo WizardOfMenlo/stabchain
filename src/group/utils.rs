02d606e5a25b58f42d6cdc30863feaaa9d6f6164
Giacomo Fenzi <giacomofenzi@outlook.com>
2020-06-14 23:26:59 +0200

    Better test

--- src/group/utils.rs
+++ src/group/utils.rs
@@ -30,7 +30,17 @@ mod tests {
     fn test_generator_orbit() {
         let g = copies_of_cyclic(&[3, 4, 5]);
         assert_eq!(g.orbit(0).len(), 3);
+        assert_eq!(g.orbit(1).len(), 3);
+        assert_eq!(g.orbit(2).len(), 3);
+        assert_eq!(g.orbit(3).len(), 4);
         assert_eq!(g.orbit(4).len(), 4);
+        assert_eq!(g.orbit(5).len(), 4);
+        assert_eq!(g.orbit(6).len(), 4);
+        assert_eq!(g.orbit(7).len(), 5);
+        assert_eq!(g.orbit(8).len(), 5);
         assert_eq!(g.orbit(9).len(), 5);
+        assert_eq!(g.orbit(10).len(), 5);
+        assert_eq!(g.orbit(11).len(), 5);
+        assert_eq!(g.orbit(12).len(), 1);
     }
 }
