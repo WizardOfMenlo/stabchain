8802eb7a4a14a11dad85c4b4cf4055e0e0f27d13
Giacomo Fenzi <giacomofenzi@outlook.com>
2020-06-17 11:58:56 +0200

    Added bench for many gens groups

--- benches/entry.rs
+++ benches/entry.rs
@@ -3,7 +3,8 @@ use criterion::criterion_main;
 mod group;
 mod perm;
 
-use group::orbit::{orbit, orbit_impl};
+use group::orbit;
+use group::orbit::orbit_impl;
 use perm::permutation;
 
 criterion_main!(permutation, orbit, orbit_impl);
