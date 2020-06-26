74269649171e81b809ea18de7271d616906df894
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-06-26 12:50:55 +0100

    Added new moved point selector for first moved point.

--- src/group/stabchain/moved_point_selector.rs
+++ src/group/stabchain/moved_point_selector.rs
@@ -1,5 +1,7 @@
 use crate::perm::Permutation;
 
+const ID_ERROR: &str = "Should never be id";
+
 /// A very small trait, used to seamlessly switch between
 /// an automatic base repr, and one which uses a precomputed one
 pub(crate) trait MovedPointSelector {
@@ -11,7 +13,7 @@ pub(crate) struct LmpSelector;
 
 impl MovedPointSelector for LmpSelector {
     fn moved_point(&mut self, p: &Permutation) -> usize {
-        p.lmp().expect("Should never be id")
+        p.lmp().expect(ID_ERROR)
     }
 }
 
@@ -35,6 +37,19 @@ impl MovedPointSelector for FixedBaseSelector {
             .expect("Base was shorter than expected")
     }
 }
+///Struct for a base point selector that takes the first point to be moved.
+pub(crate) struct FmpSelector;
+
+impl MovedPointSelector for FmpSelector {
+    fn moved_point(&mut self, p: &Permutation) -> usize {
+        //Find the first point that isn't fixed.
+        p.as_vec()
+            .iter()
+            .enumerate()
+            .position(|(i, x)| i != *x)
+            .expect(ID_ERROR)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -54,5 +69,23 @@ mod tests {
         assert_eq!(selector.moved_point(&p), 0);
         assert_eq!(selector.moved_point(&p), 1);
         assert_eq!(selector.moved_point(&p), 2);
+    }
+
+    #[test]
+    fn fmp_test() {
+        let p1 = Permutation::from_vec(vec![0, 1, 3, 2]);
+        let p2 = Permutation::from_vec(vec![1, 0]);
+        let mut selector = FmpSelector;
+        assert_eq!(selector.moved_point(&p1), 2);
+        assert_eq!(selector.moved_point(&p2), 0);
+    }
+
+    #[should_panic]
+    #[test]
+    /// The selector should not be passed the ID, and will panic otherwise.
+    fn fmp_test_id() {
+        let mut selector = FmpSelector;
+        let id = Permutation::id();
+        selector.moved_point(&id);
     }
 }
