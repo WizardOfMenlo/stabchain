38151231ef8c93336f98cb0b393c9347282fbb6f
Giacomo Fenzi <giacomofenzi@outlook.com>
2020-07-18 18:30:33 +0200

    Add tests for transversal

--- src/group/orbit/transversal/mod.rs
+++ src/group/orbit/transversal/mod.rs
@@ -40,10 +40,64 @@ where
     }
 }
 
+/// How can a transversal be invalid?
+#[derive(Debug)]
+pub enum TransversalError<P, OrbitT> {
+    /// The base is not in the orbit
+    BaseNotInOrbit(OrbitT),
+    /// An element in the orbit does not have a representative
+    MissingRepresentative(OrbitT),
+
+    /// The representative does not give the point when applied to the base
+    InvalidRepresentative(P, OrbitT),
+    /// The transversal has an invalid lenght (0)
+    InvalidLen,
+
+    /// Len of the orbit != Len of transversal
+    MismatchedLen(usize, usize),
+}
+
+/// Validate a transversal
+pub fn valid_transversal<P, A>(
+    transv: &impl Transversal<P, A>,
+) -> Result<(), TransversalError<P, A::OrbitT>>
+where
+    A: Action<P>,
+{
+    match (transv.len(), transv.orbit().len()) {
+        (0, _) => return Err(TransversalError::InvalidLen),
+        (t, o) if t != o => return Err(TransversalError::MismatchedLen(t, o)),
+        _ => {}
+    }
+
+    let base = transv.base();
+    let action = transv.action();
+    if !transv.in_orbit(base.clone()) {
+        return Err(TransversalError::BaseNotInOrbit(base));
+    }
+
+    for point in transv.orbit().iter() {
+        let repr = transv.representative(point.clone());
+
+        if repr.is_none() {
+            return Err(TransversalError::MissingRepresentative(point.clone()));
+        }
+
+        let repr = repr.unwrap();
+
+        if action.apply(&repr, base.clone()) != point.clone() {
+            return Err(TransversalError::InvalidRepresentative(repr, point.clone()));
+        }
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 macro_rules! transversal_tests {
     ($name:ty, $short:ident) => {
         mod $short {
+            use crate::group::orbit::transversal::valid_transversal;
             use crate::group::orbit::transversal::Transversal;
             use crate::group::Group;
             use crate::perm::*;
@@ -52,6 +106,7 @@ macro_rules! transversal_tests {
             fn id_transveral() {
                 let g = Group::trivial();
                 let fc = <$name>::new(&g, 3);
+                valid_transversal(&fc).unwrap();
                 assert_eq!(fc.base(), 3);
                 assert!(fc.in_orbit(3));
                 assert!(!fc.in_orbit(2));
@@ -64,6 +119,7 @@ macro_rules! transversal_tests {
             fn id_representatives() {
                 let g = Group::trivial();
                 let fc = <$name>::new(&g, 3);
+                valid_transversal(&fc).unwrap();
                 assert_eq!(DefaultPermutation::id(), fc.representative(3).unwrap());
                 assert_eq!(None, fc.representative(2))
             }
@@ -74,6 +130,7 @@ macro_rules! transversal_tests {
                 let perm = DefaultPermutation::from_images(&[0, 3, 2, 1]);
                 let g = Group::new(&[perm]);
                 let fc = <$name>::new(&g, 1);
+                valid_transversal(&fc).unwrap();
                 assert_eq!(fc.base(), 1);
                 assert!(fc.in_orbit(1));
                 assert!(fc.in_orbit(3));
@@ -89,6 +146,7 @@ macro_rules! transversal_tests {
                 let perm = DefaultPermutation::from_images(&[1, 2, 3, 0]);
                 let g = Group::new(&[perm]);
                 let fc = <$name>::new(&g, 3);
+                valid_transversal(&fc).unwrap();
                 for i in 0_usize..=3 {
                     assert!(fc.in_orbit(i));
                     assert_eq!(i, fc.representative(i).unwrap().apply(3));
