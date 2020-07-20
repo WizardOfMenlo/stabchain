4eaf4a4f44ed0647668d9d270f171382341b9613
Giacomo Fenzi <giacomofenzi@outlook.com>
2020-07-20 19:11:19 +0200

    An idea of what testing we might want to do

--- src/perm/export/mod.rs
+++ src/perm/export/mod.rs
@@ -9,12 +9,19 @@ pub use cycles::CyclePermutation;
 use super::impls::standard::StandardPermutation;
 
 /// A permutation that is easy to export
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ExportablePermutation(Vec<usize>);
 
 impl From<StandardPermutation> for ExportablePermutation {
     fn from(perm: StandardPermutation) -> Self {
         ClassicalPermutation::from(perm).into()
+    }
+}
+
+impl From<ExportablePermutation> for StandardPermutation {
+    fn from(perm: ExportablePermutation) -> Self {
+        use std::iter::FromIterator;
+        StandardPermutation::from_iter(perm.0.iter().map(|i| i - 1))
     }
 }
 
---
+++ tests/integration_tests.rs
@@ -0,0 +1,60 @@
+use std::fs::File;
+use std::io::BufReader;
+
+use lazy_static::lazy_static;
+
+use stabchain::group::Group;
+use stabchain::group::orbit::transversal::valid_transversal;
+use stabchain::group::stabchain::valid_stabchain;
+use stabchain::perm::export::ExportablePermutation;
+use stabchain::perm::DefaultPermutation;
+
+// We use this to limit the number of groups to test
+const LIMIT: usize = 1000;
+
+lazy_static! {
+    static ref GROUP_LIBRARY: Vec<Group<ExportablePermutation>> = group_library();
+}
+
+fn group_library() -> Vec<Group<ExportablePermutation>> {
+    let input = File::open("data/groups.json").unwrap();
+    let input = BufReader::new(input);
+
+    let groups: Vec<Group<ExportablePermutation>> = serde_json::from_reader(input).unwrap();
+    groups
+        .into_iter()
+        .take(LIMIT)
+        .collect()
+}
+
+#[test]
+fn test_transversals() {
+    let mut errors = 0;
+    for g in GROUP_LIBRARY.iter().cloned().map(|g| g.map(DefaultPermutation::from)) {
+        let transversal = g.transversal(0);
+        let validation = valid_transversal(&transversal);
+        if validation.is_err() {
+            errors += 1;
+            println!("{:?}", validation.unwrap_err());
+        } 
+    }
+
+    println!("{} errors out of {}", errors, LIMIT);
+    assert_eq!(errors, 0);
+}
+
+#[test]
+fn test_stabilizer() {
+    let mut errors = 0;
+    for g in GROUP_LIBRARY.iter().cloned().map(|g| g.map(DefaultPermutation::from)) {
+        let transversal = g.stabchain();
+        let validation = valid_stabchain(&transversal);
+        if validation.is_err() {
+            errors += 1;
+            println!("{:?}", validation.unwrap_err());
+        } 
+    }
+
+    println!("{} errors out of {}", errors, LIMIT);
+    assert_eq!(errors, 0);
+}
