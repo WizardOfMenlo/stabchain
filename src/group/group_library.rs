9c9ceaf98f8a4ad71e3b13e68b08389d2f254435
Giacomo Fenzi <giacomofenzi@outlook.com>
2020-07-20 21:32:51 +0200

    Docs

--- src/group/group_library.rs
+++ src/group/group_library.rs
@@ -2,8 +2,10 @@ use num::BigUint;
 use serde::{Deserialize, Serialize};
 
 use super::Group;
-use crate::perm::DefaultPermutation;
+use crate::perm::{DefaultPermutation, Permutation};
 
+/// A simple struct that keeps track of additional information of a group, such
+/// as the order. This is very useful for testing stabilizer chains
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct DecoratedGroup<P = DefaultPermutation> {
     group: Group<P>,
@@ -11,10 +13,12 @@ pub struct DecoratedGroup<P = DefaultPermutation> {
 }
 
 impl<P> DecoratedGroup<P> {
+    /// Create a new decorated group (note that this does not checks whatsover)
     pub fn new(group: Group<P>, size: BigUint) -> Self {
         Self { group, size }
     }
 
+    /// Switch the underlying group permutation type
     pub fn map<F, T>(self, func: F) -> DecoratedGroup<T>
     where
         F: FnMut(P) -> T,
@@ -25,11 +29,23 @@ impl<P> DecoratedGroup<P> {
         }
     }
 
+    /// Get the associated group
     pub fn group(&self) -> &Group<P> {
         &self.group
     }
 
+    /// Get the order of the group
     pub fn order(&self) -> &BigUint {
         &self.size
+    }
+}
+
+impl<P> From<Group<P>> for DecoratedGroup<P>
+where
+    P: Permutation,
+{
+    fn from(g: Group<P>) -> Self {
+        let order = g.stabchain().order();
+        DecoratedGroup::new(g, order)
     }
 }
