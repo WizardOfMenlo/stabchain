e8cdc0e805e53dddb63527cd61b8a819b515a307
Giacomo Fenzi <giacomofenzi@outlook.com>
2020-07-23 20:22:20 +0200

    Remove a lot of froms

--- src/perm/export/mod.rs
+++ src/perm/export/mod.rs
@@ -7,15 +7,24 @@ pub use classic::ClassicalPermutation;
 pub use cycles::CyclePermutation;
 
 use super::impls::standard::StandardPermutation;
-use super::impls::sync::SyncPermutation;
+use crate::perm::Permutation;
 
 /// A permutation that is easy to export
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ExportablePermutation(Vec<usize>);
 
-impl From<StandardPermutation> for ExportablePermutation {
-    fn from(perm: StandardPermutation) -> Self {
-        ClassicalPermutation::from(perm).into()
+impl<P> From<P> for ExportablePermutation
+where
+    P: Permutation,
+{
+    fn from(perm: P) -> Self {
+        ClassicalPermutation::from(StandardPermutation::from_images(&perm.images()[..])).into()
+    }
+}
+
+impl From<ClassicalPermutation> for ExportablePermutation {
+    fn from(perm: ClassicalPermutation) -> Self {
+        ExportablePermutation(perm.images())
     }
 }
 
@@ -23,23 +32,5 @@ impl From<ExportablePermutation> for StandardPermutation {
     fn from(perm: ExportablePermutation) -> Self {
         use std::iter::FromIterator;
         StandardPermutation::from_iter(perm.0.iter().map(|i| i - 1))
-    }
-}
-
-impl From<SyncPermutation> for ExportablePermutation {
-    fn from(perm: SyncPermutation) -> Self {
-        StandardPermutation::from(perm).into()
-    }
-}
-
-impl From<ExportablePermutation> for SyncPermutation {
-    fn from(perm: ExportablePermutation) -> Self {
-        StandardPermutation::from(perm).into()
-    }
-}
-
-impl From<ClassicalPermutation> for ExportablePermutation {
-    fn from(perm: ClassicalPermutation) -> Self {
-        ExportablePermutation(perm.images())
     }
 }
