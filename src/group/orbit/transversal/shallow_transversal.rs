1ccf57e268a8a4b94ab96abea5d5f36092178331
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-07-31 13:22:29 +0100

    added tests

--- src/group/orbit/transversal/shallow_transversal.rs
+++ src/group/orbit/transversal/shallow_transversal.rs
@@ -8,7 +8,7 @@ use std::collections::{HashMap, VecDeque};
 use std::iter::once;
 
 //Make a transversal with depths at most max_depth + 1.
-pub(crate) fn random_transversal<P, A, R>(
+pub fn random_transversal<P, A, R>(
     g: &mut Group<P>,
     base: A::OrbitT,
     strat: &A,
@@ -52,6 +52,7 @@ where
         }
     }
     let max_depth = depths.values().into_iter().max().unwrap().clone();
+    dbg!(&max_depth);
     if max_depth > set_depth {
         //TODO perhaps use a more efficient method than creating the entire thing.
         let new_gen = RandPerm::new(10, g, 50, rng.clone()).random_permutation();
@@ -62,5 +63,104 @@ where
         random_transversal(g, base, strat, rng, set_depth)
     } else {
         (transversal, max_depth)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::group::orbit::transversal::factored_transversal::representative_raw;
+    use crate::perm::actions::SimpleApplication;
+    use crate::perm::export::CyclePermutation;
+    use crate::perm::{DefaultPermutation, Permutation};
+
+    /// Test the factored transversal calculation for a generating set with multiple generators.
+    #[test]
+    fn multiple_generators() {
+        // Cycle notation is used for conveninece, but we do need to switch to 0 indexed for assertions.
+        let gens: Vec<DefaultPermutation> = vec![
+            CyclePermutation::from_vec(vec![vec![1, 6, 4, 3], vec![2, 7, 5]]).into(),
+            CyclePermutation::from_vec(vec![vec![1, 4], vec![2, 6, 3]]).into(),
+        ];
+        let g = Group::from_list(gens);
+        let mut rng = rand::thread_rng();
+        let set_depth = 3;
+        let strat = SimpleApplication::default();
+        //All points should be in the orbit (according to GAP)
+        for i in 0_usize..6 {
+            let (transversal, max_depth) =
+                random_transversal(&mut g.clone(), i, &strat, &mut rng, set_depth);
+            assert!(max_depth < set_depth + 1);
+            for j in 0_usize..6 {
+                assert!(transversal.contains_key(&j));
+                assert_eq!(
+                    j,
+                    representative_raw(&transversal, i, j, &strat)
+                        .unwrap()
+                        .apply(i)
+                );
+            }
+        }
+    }
+
+    /// Test the factored transversal calculation for a generating set with multiple generators
+    /// when not all elements are in the orbit.
+    #[test]
+    fn multiple_generators_non_full_orbit() {
+        use crate::perm::export::CyclePermutation;
+        // Cycle notation is used for conveninece, but we do need to switch to 0 indexed for assertions.
+        let gens: Vec<DefaultPermutation> = vec![
+            CyclePermutation::single_cycle(&[1, 2, 6]).into(),
+            CyclePermutation::single_cycle(&[3, 5, 7]).into(),
+        ];
+        let g = Group::from_list(gens);
+        let mut rng = rand::thread_rng();
+        let set_depth = 3;
+        let strat = SimpleApplication::default();
+        let (fc1, max_depth) = random_transversal(&mut g.clone(), 5, &strat, &mut rng, set_depth);
+        assert_eq!(3, fc1.len());
+        let (fc2, max_depth) = random_transversal(&mut g.clone(), 4, &strat, &mut rng, set_depth);
+        assert_eq!(3, fc2.len());
+        let (fc3, max_depth) = random_transversal(&mut g.clone(), 3, &strat, &mut rng, set_depth);
+        assert_eq!(1, fc3.len());
+        assert!(max_depth < set_depth + 1);
+        for i in [0, 1, 5].iter() {
+            // Tests for fc1
+            assert!(fc1.contains_key(i));
+            assert_eq!(
+                *i,
+                representative_raw(&fc1, 5, *i, &strat).unwrap().apply(5)
+            );
+            // Tests for fc2
+            assert!(!fc2.contains_key(i));
+            assert_eq!(None, representative_raw(&fc2, 4, *i, &strat));
+            // Tests for fc3
+            assert!(!fc3.contains_key(i));
+            assert_eq!(None, representative_raw(&fc3, 3, *i, &strat));
+        }
+        for i in [2, 4, 6].iter() {
+            // Tests for fc1
+            assert!(!fc1.contains_key(i));
+            assert_eq!(None, representative_raw(&fc1, 5, *i, &strat));
+            // Tests for fc2
+            assert!(fc2.contains_key(i));
+            assert_eq!(
+                *i,
+                representative_raw(&fc2, 4, *i, &strat).unwrap().apply(4)
+            );
+            // Tests for fc3
+            assert!(!fc3.contains_key(i));
+            assert_eq!(None, representative_raw(&fc3, 3, *i, &strat));
+        }
+        // Now to test for element 3
+        // Tests for fc1
+        assert!(!fc1.contains_key(&3));
+        assert_eq!(None, representative_raw(&fc1, 5, 3, &strat));
+        // Tests for fc2
+        assert!(!fc2.contains_key(&3));
+        assert_eq!(None, representative_raw(&fc2, 4, 3, &strat));
+        // Tests for fc3
+        assert!(fc3.contains_key(&3));
+        assert_eq!(3, representative_raw(&fc3, 3, 3, &strat).unwrap().apply(3));
     }
 }
