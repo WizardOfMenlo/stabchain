ee8e06ea5c04ee9eb0cb8bbbb6353af2eadee51a
EwanGilligan <32061818+EwanGilligan@users.noreply.github.com>
2021-06-18 15:42:55 +0100

    Depreciate Naïve Random (#91)
    * Mark RandomBuilderStrategyNaive as Depreciated
    
    It is worse than Shallow Random, and slows down tests and benchmarks
    
    * Remove benchmarks for random naive
    
    * Remove full test for random
    
    * Remove integration tests for random

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -175,22 +175,6 @@ test_stabilizer_on_strategy!(
 );
 
 test_stabilizer_on_strategy!(
-    RandomBuilderStrategyNaive::new(SimpleApplication::default(), FmpSelector::default(),),
-    test_random_stabilizer,
-    (number_of_tests() as f32 * 0.05).floor() as usize
-);
-test_stabilizer_on_strategy!(
-    RandomBuilderStrategyNaive::new_with_params(
-        SimpleApplication::default(),
-        FmpSelector::default(),
-        RandomAlgoParameters::default().quick_test(true)
-    ),
-    test_random_quick_test,
-    //Bound so high as this is very unreliable.
-    (number_of_tests() as f32 * 0.15).floor() as usize
-);
-
-test_stabilizer_on_strategy!(
     RandomBuilderStrategyShallow::new(SimpleApplication::default(), FmpSelector::default(),),
     test_random_shallow_stabilizer,
     (number_of_tests() as f32 * 0.05).floor() as usize
@@ -204,18 +188,6 @@ test_stabilizer_on_strategy!(
     ),
     test_random_shallow_stabilizer_quick_test,
     (number_of_tests() as f32 * 0.05).floor() as usize
-);
-
-test_stabilizer_on_strategy_with_order!(
-    |order| RandomBuilderStrategyNaive::new_with_params(
-        SimpleApplication::default(),
-        FmpSelector::default(),
-        RandomAlgoParameters::default()
-            .quick_test(true)
-            .order(order)
-    ),
-    test_random_known_order,
-    0
 );
 
 test_stabilizer_on_strategy_with_order!(
